ff397da7f3963aeff740390feac9ac3696fddbc7
b4baxxx <syiffamuqaddimah_2226240143@mhs.mdp.ac.id>
2024-10-24 09:32:45 +0700

    update

---
+++ android/app/src/main/java/com/example/wisata_candi_syifaa/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.wisata_candi_syifaa;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/example/wisata_candi_syifaa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wisata_candi_syifaa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
