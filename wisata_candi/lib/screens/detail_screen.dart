e1a6794b51385b2ab9b0572956af05ded498410d
b4baxxx <syiffamuqaddimah_2226240143@mhs.mdp.ac.id>
2024-10-24 09:27:19 +0700

    new

---
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key, required Candi candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      )
+    );
+  }
+}
